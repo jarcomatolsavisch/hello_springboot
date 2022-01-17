bab9124a74fc1b92b447afa595f9148188af13d6
jarcomatolsavisch <ma6036tp@gmail.com>
2022-01-17 11:24:52 +0800

    First commit

---
+++ src/main/java/com/jamesma/hello_world/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.jamesma.hello_world;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello, Spring boot";
+    }
+}
